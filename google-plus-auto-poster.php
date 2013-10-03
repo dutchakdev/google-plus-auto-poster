a70fdd0916ff7eb9a5b4c0c838c27b221b5284be
Subhash8108 <Subhash8108@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-03 12:26:14 +0000

    Version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/google-plus-auto-poster/tags/1.0@781818 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ google-plus-auto-poster.php
@@ -0,0 +1,152 @@
+<?php 
+
+  /*
+   Plugin Name: Google Plus Auto Poster
+   Plugin URI: http://www.wpsuperplugin.com
+   Description: This plugin automatically publishes posts from on your google profile page
+   Version: 1.0
+   Author: Mr.Subhash Patel
+   Author URI: http://www.wpsuperplugin.com
+   */
+  
+function wpsp_googleplus_deactiveplugin()
+{
+	global $wpdb;
+	//delete_option('wpsp_gpapoption');
+	
+}
+register_deactivation_hook( __FILE__, 'wpsp_googleplus_deactiveplugin' );
+
+
+//Drop Database Table
+function wpsp_googleplus_dropplugin()
+{
+	global $wpdb;
+	delete_option('wpsp_gpapoption');
+
+}
+register_uninstall_hook( __FILE__, 'wpsp_googleplus_dropplugin' );
+
+
+function wpsp_gmail_menu()
+{
+	//icon display on side title plugin in leftside
+    $icon_url=WP_PLUGIN_URL."/google-plus-auto-poster/images/googleplusicone.png";
+	add_menu_page('WPSP_Googlepost_Autopost', 'Google+Autopost', 'activate_plugins', 'wpspgpap_authontication', 'wpspgpap_authon',$icon_url);
+	
+}
+add_action('admin_menu', 'wpsp_gmail_menu');
+
+function wpspgpap_authon()
+{
+	$wpapgetoption =get_option('wpsp_gpapoption'); 	
+	if(($_POST['username']!="" && $_POST['password']!="")) 
+	{
+		if($_POST['username']!="" && $_POST['password']!="")         
+		{     
+			require_once('apigoogleplus.php');
+		        
+			//Option Value                                 
+			if ( get_option( 'wpsp_gpapoption' ) !== false  )   update_option( 'wpsp_gpapoption' , $_POST );            else                                                add_option('wpsp_gpapoption' , $_POST );       
+			
+			require_once('authontication.php'); 
+		}
+	}
+	?>
+<link href="<?php echo WP_PLUGIN_URL."/google-plus-auto-poster/css/wpspgoogleplus.css"; ?>" rel="stylesheet" type="text/css" />
+<div style="margin-top:20px;">
+<?php 
+$statusgpap =get_option( 'wpsp_gpapoptionstatus');
+		if($statusgpap=="success" && $msg=="")
+		{
+			?><div id="msg"><div class="success">Login Success</div></div><?php
+		}
+		if($statusgpap!="success" && $msg=="")
+		{
+			?><div id="msg"><div class="error">Incorrect Username/Password </div></div><?php
+		}
+?>
+<div id="msg"><?php echo $msg; ?></div>
+<div id="googleboxes">
+  <div class="googleplusbody">
+  <?php
+  wp_enqueue_script("authvalidationjs", plugins_url( '/js/jquery.validate.js' , __FILE__ ), array("jquery"));
+  wp_enqueue_script("authjs", plugins_url( '/js/authontication_check.js' , __FILE__ ), array("jquery"));
+  ?>
+    <h2>Google Plus Auto Post</h2>
+    <div>
+      <form action="admin.php?page=wpspgpap_authontication" method="post" name="authform" id="authform">
+        <table class="form-table" width="100%">
+          <tr valign="top">
+            <th scope="row">Email Address:</th>
+            <td><input type="text" name="username" class="googleforminput" id="username" value="<?php echo $wpapgetoption['username']; ?>" /></td>
+          </tr>
+          <tr valign="top">
+            <th scope="row">Password:</th>
+            <td><input type="password" name="password" class="googleforminput" id="password" value="<?php echo $wpapgetoption['password']; ?>" /></td>
+          </tr>
+        </table>
+        <p class="submit">
+          <input type="submit" name="submitauth"  class="button-primary extbutton" value="Submit" />
+        </p>
+      </form>
+    </div>
+  </div>
+</div>
+</div>
+<!--   <div><button name="synchronize" id="synchronize" class="button-primary" >Test</button></div>-->
+<?php
+}
+
+global $post;
+
+function wpspgpap()
+{	
+	global $wpdb;
+	global $post;
+	
+	$my_postid = $post->ID;//This is page id or post id
+	$gettype =get_post_type($my_postid);
+	
+	if($gettype=='post')
+	{
+		require_once('apigoogleplus.php');
+		$content_post = get_post($my_postid);
+		$gpap_guid =$content_post->guid;
+		$gpap_title =$content_post->post_title;
+		$message = $content_post->post_content;
+		$message = apply_filters('the_content', $message);
+		$message = str_replace(']]>', ']]&gt;', $message);
+		
+		$statusgpap =get_option( 'wpsp_gpapoptionstatus');
+		if($statusgpap=="success")
+		{
+			require_once('authontication.php');
+					
+			//Login success then call if condition
+			if (!$loginconnection)
+			{
+				// link with page title display
+				$thumbnilcheck = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+				
+				if($thumbnilcheck!="")
+				{
+					$link = array('img'=>$thumbnilcheck,'link'=>$gpap_guid,'domain'=>'','title'=>$gpap_title,'txt'=>'');
+					$contentsss = get_post_field('post_content', $my_postid);
+					PostOnGooglePlus($contentsss,$link);	
+					
+				}
+				else
+				{
+					$link = getlinkandtitle($content_post->guid);
+					$contentsss = get_post_field('post_content', $my_postid);
+					PostOnGooglePlus($contentsss,$link);	
+					 
+				}
+			}
+		}
+	}
+}
+add_action('publish_post', 'wpspgpap');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,89 @@
+=== Google Plus Auto Poster ===
+Contributors: Google Plus Auto Poster
+Donate link: http://www.wpsuperplugin.com/download/google-auto-poster/
+Tags: google plus, google plus auto poster, google plus post, google plus auto post, google plus auto share, social network auto poster, google plus wordpress Plugin, google plus auto poster Plugin, google plus auto share Plugin, automation, autopost, auto-post, auto post, socialnetworks, socialnetwork, social networks, social network, google+, auto post to google plus wordpress
+Requires at least: 3.6
+Tested up to: 3.6
+Stable tag: 4.3This plugin automatically publishes posts from on your google profile page
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Automatically re-publishes blogposts to Google+ profiles and/or pages
+
+== Description ==
+
+**This plugin automatically publishes posts from your blog to your Social Network accounts**
+
+Google plus Auto Poster (GPAP) is Social networks wordpress plugin that gives you an ability to automatically re-post your posts or 
+announcements of your posts to most popular Social Network site Google Plus. With the Use of GPAP you can save your valuable 
+time and connect with friends and relative easily using GPAP. You have to just write the post and API will do the rest. 
+
+Google Plus auto poster special used for wordpress. **This plugin through easy and fast auto sharing in google plus**
+
+GPAP Auto Post for to following pages
+<ul>
+<li>Google plus profile</li>
+<li>Google plus Business Pages</li>
+<li>Google plus Communities</li>
+</ul>
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `wpsp-googleplus` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Alternatively, you can install and activate the plugin through the WordPress plugin manager.
+ 
+= Requirements =
+Following requirement is very important other vise not working google+ auto post
+<ul>
+<li>PHP5</li>
+<li>cURL</li>
+<li>OpenSSL</li>
+</ul>
+
+For more detail please vist our website <a href="http://wpsuperplugin.com/">http://wpsuperplugin.com/</a>
+
+== Frequently Asked Questions ==
+
+= What is the difference between "Free" and "Pro" versions? =
+
+Free Plugin is limited access means only automatically post on google profile page and when Pro plugin is not only automatically
+post on google profile page with google business page and google communites page
+
+= Why google plus auto poster are not fully free? =
+
+Google+ and Pinterest don't have API or have a read-only API that doesn't allow to make posts. We had to create our own API 
+libraries for google+ networks and those libraries are available in google plus auto poster
+
+
+== Screenshots ==
+
+1. Plugin activate after look page.
+2. Jquery Validation in form.
+3. Incorrect Username and Password.
+4. Second step validation because auto poster in wordpress.
+5. Login success.
+6. Create New Post.
+7. Share your wordpress post on google profile page with link and message.
+8. Create New Post With feature image.
+9. Share your wordpress post on google profile page with image,link and message.
+
+For more screenshot please vist our website <a href="http://www.wpsuperplugin.com/screen-shot/">http://www.wpsuperplugin.com/screen-shot/</a>
+
+
+== Changelog ==
+
+= 1.0 =
+* Ability to share your post in google plus profile page
+
+== Upgrade Notice ==
+
+Just replace plugin files, the rest will be updated automatically.
+
+
+== Arbitrary section ==
+
+
+== A brief Markdown Example ==
